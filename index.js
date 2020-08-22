93bd9b9284ba867b978b6f9af67d8cdd3a041a30
wbarobinson <wbarobinson@gmail.com>
2020-08-22 13:57:36 -0400

    oAuth reference error

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { Autohook } = require('twitter-autohook');
 
+
 (async start => {
   try {
     const webhook = new Autohook();
     
+    webhook.on('event', async event => {
+      if (event.direct_message_events) {
+        await sayHi(event);
+      }
+      console.log('You received an event!');
+    });
     // Removes existing webhooks
     await webhook.removeWebhooks();
     
@@ -18,3 +25,45 @@ const { Autohook } = require('twitter-autohook');
     process.exit(1);
   }
 })(); 
+
+async function sayHi(event) {
+  // We check that the message is a direct message
+  if (!event.direct_message_events) {
+    return;
+  }
+
+  // Messages are wrapped in an array, so we'll extract the first element
+  const message = event.direct_message_events.shift();
+
+  // We check that the message is valid
+  if (typeof message === 'undefined' || typeof message.message_create === 'undefined') {
+    return;
+  }
+
+  // We filter out message you send, to avoid an infinite loop
+  if (message.message_create.sender_id === message.message_create.target.recipient_id) {
+    return;
+  }
+
+  // Prepare and send the message reply
+  const senderScreenName = event.users[message.message_create.sender_id].screen_name;
+
+  const requestConfig = {
+    url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
+    oauth: oAuthConfig,
+    json: {
+      event: {
+        type: 'message_create',
+        message_create: {
+          target: {
+            recipient_id: message.message_create.sender_id,
+          },
+          message_data: {
+            text: `Hi @${senderScreenName}! 👋`,
+          },
+        },
+      },
+    },
+  };
+  await post(requestConfig);
+}
