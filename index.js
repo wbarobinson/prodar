c72f5e108bbc46cefe99861568d1f2e1ab7ab2d2
wbarobinson <wbarobinson@gmail.com>
2020-08-22 15:32:34 -0400

    d

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
-const { Autohook } = require('twitter-autohook');
+const { Autohook, setWebhook, validateWebhook, WebhookURIError, RateLimitError } = require('twitter-autohook');
+const util = require('util');
+const request = require('request');
+
+const post = util.promisify(request.post);
+
+const oAuthConfig = {
+  token: process.env.TWITTER_ACCESS_TOKEN,
+  token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+};
+
+var coordArray = [];
+
+async function sayHi(event) {
+   // We check that the message is a direct message
+   if (!event.direct_message_events) {
+    return;
+  }
+
+  // Messages are wrapped in an array, so we'll extract the first element
+  const message = event.direct_message_events.shift();
+
+  // We check that the message is valid
+  if (typeof message === 'undefined' || typeof message.message_create === 'undefined') {
+    return;
+  }
+ 
+  // We filter out message you send, to avoid an infinite loop
+  if (message.message_create.sender_id === message.message_create.target.recipient_id) {
+    return;
+  }
+
+  // Prepare and send the message reply
+  const senderScreenName = event.users[message.message_create.sender_id].screen_name;
+
+
+
+  const requestConfig = {
+    url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
+    oauth: oAuthConfig,
+    json: {
+      event: {
+        type: 'message_create',
+        message_create: {
+          target: {
+            recipient_id: message.message_create.sender_id,
+          },
+          message_data: {
+            text: `Hi @${senderScreenName}! ${coordArray} 👋`,
+          },
+        },
+      },
+    },
+  };
+  await post(requestConfig);
+}
 
 (async start => {
   try {
     const webhook = new Autohook();
-    
     webhook.on('event', async event => {
       if (event.direct_message_events) {
-        
-        async function sayHi(event) {
-          // We check that the message is a direct message
-          if (!event.direct_message_events) {
-            return;
-          }
-        
-          // Messages are wrapped in an array, so we'll extract the first element
-          const message = event.direct_message_events.shift();
-        
-          // We check that the message is valid
-          if (typeof message === 'undefined' || typeof message.message_create === 'undefined') {
-            return;
-          }
-        
-          // We filter out message you send, to avoid an infinite loop
-          if (message.message_create.sender_id === message.message_create.target.recipient_id) {
-            return;
-          }
-        
-          // Prepare and send the message reply
-          const senderScreenName = event.users[message.message_create.sender_id].screen_name;
-        
-          const requestConfig = {
-            url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
-            oauth: {
-              consumer_key: 'pufmcF3kGmpDVFcvDbbYaengK',
-              consymer_secret: '3r6hCGu2jmxczzhhl0CNwHnFaHHCNon87sLNFR40124OmnZbc4',
-              token: '24737459-topbrUHGWHQkZXfMqctnOQ77XwAdr9ERAhlUOKG0s',
-              token_secret: 'MgAfpJAkhnGKJzBQ3AmBPVdya5GCnStiwPMrKzYdOIryc',
-            },
-            json: {
-              event: {
-                type: 'message_create',
-                message_create: {
-                  target: {
-                    recipient_id: message.message_create.sender_id,
-                  },
-                  message_data: {
-                    text: `Hi @${senderScreenName}! 👋`,
-                  },
-                },
-              },
-            },
-          };
-          await post(requestConfig);
-        }
         await sayHi(event);
       }
-      console.log('You received an event!');
     });
     // Removes existing webhooks
     await webhook.removeWebhooks();
@@ -70,5 +78,4 @@ const { Autohook } = require('twitter-autohook');
     console.error(e);
     process.exit(1);
   }
-})(); 
-
+})();  
